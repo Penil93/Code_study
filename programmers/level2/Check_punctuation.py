749856e7ffb78c48d52b4c993a8bec14fc2457dc
Penil93 <k0611c@gmail.com>
2022-11-24 12:21:10 +0000

    Check punctuation

---
+++ programmers/level2/Check_punctuation.py
@@ -0,0 +1,14 @@
+# 22.11.24
+# Programmers - Checking punctuation(괄호 회전하기)
+# https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+def solution(s):
+    PUNCTUATION_List = list()
+    for index, text in enumerate(s):
+        if text == "(": PUNCTUATION_List.append(index)
+        elif text == ")":
+            if len(PUNCTUATION_List) == 0: return False
+            else: PUNCTUATION_List.pop(-1)
+        else: continue
+    if len(PUNCTUATION_List) == 0: return True
+    else: return False
